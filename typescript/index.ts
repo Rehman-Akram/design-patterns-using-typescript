ccc9ac9180d36b9f455733466c84a560a84ff680
RehmanAkram <rehmanakram03@gmail.com>
2024-07-05 12:56:33 +0500

    abstraction example

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,4 +1,15 @@
-import {secondFunction} from './second.js'
+/* Abstraction, Its provide a technique for hiding all the complexities of implementation and provide user with
+the abstracted view to use things regardless there working details. 
+*/
 
-console.log("yuppie")
-secondFunction()
+//Example, date class is providing methods to get year, day, month etc. and hiding all the details of implementaion.
+const date = new Date()
+
+// current year
+console.log(date.getFullYear())
+
+// currnet month
+console.log(date.getMonth()+1)
+console.log(date.getFullYear)
+// current date
+console.log(date.getDate())
--- typescript/second.ts
+++
@@ -1,3 +0,0 @@
-export const secondFunction = (): void => {
-  console.log('second')
-}
